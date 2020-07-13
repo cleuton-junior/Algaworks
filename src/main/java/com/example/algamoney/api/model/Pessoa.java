61e1e28fb5d1b8f9174734c4a0a455c784fcf87b
cleuton-junior <barcleuton@gmail.com>
2020-07-13 14:16:54 -0300

    Curso de Angular/SpringBoot

---
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -0,0 +1,89 @@
+package com.example.algamoney.api.model;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long codigo;
+	
+	@NotNull
+	private String nome;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	@NotNull
+	private Boolean ativo;
+	
+	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ativo == null) ? 0 : ativo.hashCode());
+		result = prime * result + (int) (codigo ^ (codigo >>> 32));
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (ativo == null) {
+			if (other.ativo != null)
+				return false;
+		} else if (!ativo.equals(other.ativo))
+			return false;
+		if (codigo != other.codigo)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+}
